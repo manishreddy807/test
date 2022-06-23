28aa50fe1fa5029da22d83e0c989f4ae95ff0adc
manishreddy807 <manishreddy807@gmail.com>
2022-06-23 18:42:59 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import HomePage from './components/HomePage';
+import OneSignal from 'react-onesignal';
 // import ComponentTriggered from './components/Language';
 // import ComponentRender from './components/Language/new';
 
  
 export default function App () {
+const [initialized, setInitialized] = useState(false)
+useEffect(() => {
+  OneSignal.init({
+    appId: ""
+  })
+  .then(() => setInitialized(true));
+  OneSignal.showSlidedownPrompt().then(() => {
 
-
+  })
+}, [])
  return(
    <>
     <HomePage />
